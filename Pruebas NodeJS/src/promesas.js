ed51f667dbf69e18e05e7406c578a7d25bfea919
Pablo Gómez Meléndez <pablogomezmelendez@gmail.com>
2021-03-18 23:50:52 +0100

    modificación de promesas

--- Pruebas NodeJS/src/promesas.js
+++ Pruebas NodeJS/src/promesas.js
@@ -13,16 +13,16 @@ async function add1(x) {
     return x + a + b;
 }
 
-add1(10).then(v => {
-    console.log(v); // prints 30 after 4 seconds.
-});
-
-
 async function add2(x) {
     const p_a = resolveAfter2Seconds(20);
     const p_b = resolveAfter2Seconds(x);
     return x + await p_a + await p_b;
 }
+
+
+add1(10).then(v => {
+    console.log(v); // prints 30 after 4 seconds.
+});
 
 add2(20).then(v => {
     console.log(v); // prints 60 after 2 seconds.
